930cad7255222836110a47ffc025e4655684fb2f
Sylvoo <21sylwek@gmail.com>
2025-10-13 16:02:45 +0200

    Exercise 4, done, charDetect() function

---
+++ Programowanie_Obiektowe/Lab2/Ex4.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cctype> // islower() itd
+
+int charDetect(const std::string str, char a)
+{
+    int num = 0;
+    int len = str.length();
+    char temp = NULL;
+    //std::cout<<"Lenght: "<<len;
+    if(isupper(a))
+    {
+        temp = tolower(a);
+    }
+    else temp = toupper(a);
+
+    for(int i = 0; i<len; i++)
+    {
+        if(str[i] == a || str[i] == temp)
+        {
+            num++;
+        }
+    }
+
+    return num;
+}
+
+
+
+int main()
+{
+    std::string userInput;
+    char userChar;
+    std::cout << "Enter a sentence: ";
+    getline(std::cin, userInput);
+    std::cout << "\nEnter the char to check it's apperances: ";
+    std::cin >> userChar;
+
+    std::cout<<"\nNumber of apperances: "<<charDetect(userInput, userChar);
+
+    return 0;
+}
