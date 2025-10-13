fc5f660f689a992401defc5cca1284bac22a58d9
Sylvoo <21sylwek@gmail.com>
2025-10-13 14:19:56 +0200

    Declaration changes

--- Programowanie_Obiektowe/Lab2/Ex1.cpp
+++ Programowanie_Obiektowe/Lab2/Ex1.cpp
@@ -4,6 +4,44 @@
 #include <cstdlib>
 // program zliczajacy sume nieparzystych wierszy i parzystych kolumn macierzy
 
+
+std::vector<int> calcMatrix(std::vector<std::vector<int>> vec); //deklaracja funkcji 
+
+int main()
+{
+    // initialize random number generator with current time
+    srand(static_cast<unsigned int>(time(0)));
+    
+
+    int lines, column;
+    std::cout<< "Give me a number of lines: ";
+    std::cin >> lines;
+    std::cout<< "\nGive me a number of columns: ";
+    std::cin >> column;
+    std::vector<std::vector<int>> matrix(lines,std::vector<int>(column,0));
+    //std::cout<<"\n"<<randomNumber;
+    //std::cout<<matrix.size();
+
+    for (auto &row : matrix)
+    {
+        std::cout<<"\n";
+        for (auto &i : row)
+        {
+            // generate random number from 0 to 9
+            i = rand() % 10;
+            std::cout<< i <<" ";
+        }
+    }
+
+    std::cout<<"\n--------------------------------------------\n";
+    std::vector<int> answer = calcMatrix(matrix);
+    std::cout<< "Suma nieparzystych wierszy: "<<answer[0];
+    std::cout<< "\nSuma parzystych kolumn: "<<answer[1];
+
+
+    return 0;
+}
+
 std::vector<int> calcMatrix(std::vector<std::vector<int>> vec)
 {
     int sumLines = 0;
@@ -45,40 +83,4 @@ std::vector<int> calcMatrix(std::vector<std::vector<int>> vec)
     sum.push_back(sumColumns);
 
     return sum;
-}
-
-
-int main()
-{
-    // initialize random number generator with current time
-    srand(static_cast<unsigned int>(time(0)));
-    
-
-    int lines, column;
-    std::cout<< "Give me a number of lines: ";
-    std::cin >> lines;
-    std::cout<< "\nGive me a number of columns: ";
-    std::cin >> column;
-    std::vector<std::vector<int>> matrix(lines,std::vector<int>(column,0));
-    //std::cout<<"\n"<<randomNumber;
-    //std::cout<<matrix.size();
-
-    for (auto &row : matrix)
-    {
-        std::cout<<"\n";
-        for (auto &i : row)
-        {
-            // generate random number from 0 to 9
-            i = rand() % 10;
-            std::cout<< i <<" ";
-        }
-    }
-
-    std::cout<<"\n--------------------------------------------\n";
-    std::vector<int> answer = calcMatrix(matrix);
-    std::cout<< "Suma nieparzystych wierszy: "<<answer[0];
-    std::cout<< "\nSuma parzystych kolumn: "<<answer[1];
-
-
-    return 0;
 }
