876972883bc978c52c2773be24ae048ce7a18cc8
Sylwester <87396070+Sylvoo@users.noreply.github.com>
2025-11-26 08:41:45 +0100

    Refactor Rectangle constructors and add getters
    Refactored Rectangle constructors and added getters for dimensions.

--- Lab4/zad2/Rectangle.hpp
+++ Lab4/zad2/Rectangle.hpp
@@ -10,13 +10,15 @@ private:
 
 protected:
     // parametrized constructor (why this one is protected?)
-    Rectangle(double a, double b, const Point &p, std::string name) :  m_a(a), m_b(b), Figure(p, name){};
+    Rectangle(double a, double b, const Point &p, std::string name) :  m_a(a), m_b(b), Figure(p, name){
+        //...
+    };
 
 
 
 public:
     // parametrized constructor
-    Rectangle(double a, double b, const Point &p) :m_a(a), m_b(b), Figure(p,"Rectangle") {};
+    Rectangle(double a, double b, const Point &p) : Rectangle(a,b, p, "Rectangle") {};
    
     /* TODO override the area() function properly */
     double area() const override
@@ -35,18 +37,22 @@ public:
 
     void setA(double a)
     {
-        {
-            m_a = abs(a);
-        }
-        
+        m_a = abs(a);
     }
     
 
     void setB(double b)
     {
-        {
-            m_b = abs(b);
-        }
-        
+        m_b = abs(b);
+    }
+
+    double getA() const
+    {
+        return m_a;
+    }
+
+    double getB() const 
+    {
+        return m_b;
     }
 };
